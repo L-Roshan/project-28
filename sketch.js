9bda671311c940ec1f2b09479381ce2a2dc47e89
L-Roshan <77386474+L-Roshan@users.noreply.github.com>
2021-03-29 22:03:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1;
+var world,boy;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	chain = new Chain(stone.body,{x:160, y:500});
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  chain.display();
+  
+
+  treeObj.display();
+  mango1.display();
+
+  detectCollision(stone, mango1);
+  detectCollision(stone, mango2);
+  detectCollision(stone, mango3);
+  detectCollision(stone, mango4);
+  detectCollision(stone, mango5);
+  detectCollision(stone, mango6);
+  detectCollision(stone, mango7);
+
+  groundObject.display();
+}
+function mouseDragged(){
+    Matter.Body.setPosition(stone.body,{x:mouseX, y:mouseY});
+}
+function mouseReleased(){
+    chain.fly();
+}
+function keyPressed(){
+  if(keyCode === 32){
+    Matter.Body.setPosition(stone.body,{x:160, y:500});
+    chain.attach(stone.body);
+  }
+}
+function detectCollision(lstone,lmango){
+  stoneBodyPosition = lstone.body.position;
+  mangoBodyPosition = lmango.body.position;
+
+  var distance = dist(stoneBodyPosition.x, stoneBodyPosition.y, mangoBodyPosition.x, mangoBodyPosition.y);
+  if(distance <= lmango.r + lstone.r){
+    Matter.Body.setStatic(lmango.body, false);
+  }
+
+}
